18d5e7c13a0969eb97e037a604f7dfaced25d05c
unknown <pschmidt@pasit.nl>
2023-10-18 15:31:25 +0200

    Last

---
+++ day3/Integriteit/Program.cs
@@ -0,0 +1,97 @@
+﻿
+using System.Configuration.Assemblies;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Integriteit;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var doc = "Hello World";
+        // byte[] hash = GenerateHash(doc);
+        // bool isOk = Validate(doc, hash);
+
+        //byte[] hash = GenerateHMAC(doc);
+        //bool isOk = ValodateHMAC(doc, hash);
+
+        (byte[] hash, string pub) data = GenerateAsym(doc);
+        bool isOk = ValidateAsym(doc, data);
+
+
+        Console.WriteLine($"Document is {isOk}");
+    }
+
+    private static bool ValidateAsym(string doc, (byte[] hash, string pub) pakket)
+    {
+        var alg = DSA.Create();
+        alg.FromXmlString(pakket.pub);
+        var aha = SHA1.Create();
+        byte[] data = Encoding.UTF8.GetBytes(doc);    
+        byte[] hash = aha.ComputeHash(data);
+
+        return alg.VerifyData(hash, pakket.hash, HashAlgorithmName.SHA1);
+    }
+
+    private static (byte[] hash, string pub) GenerateAsym(string doc)
+    {
+        var alg = DSA.Create();
+        var pub = alg.ToXmlString(false);
+        var aha = SHA1.Create();
+        byte[] data = Encoding.UTF8.GetBytes(doc);    
+        byte[] hash = aha.ComputeHash(data);
+
+        byte[] signature = alg.SignData(hash, HashAlgorithmName.SHA1);
+        return (signature, pub);
+    }
+
+    private static bool ValodateHMAC(string doc, byte[] hash)
+    {
+        var alg = new HMACSHA1();
+        alg.Key = Encoding.UTF32.GetBytes("Pa$$w0rd");
+        byte[] data = Encoding.UTF8.GetBytes(doc);      
+        byte[] hash2 = alg.ComputeHash(data);
+        if (hash.Length != hash2.Length) return false;
+        for(int i = 0; i < hash2.Length; i++) 
+        {
+            if (hash[i] != hash2[i]) return false;
+        }
+        return true;
+    }
+
+    private static byte[] GenerateHMAC(string doc)
+    {
+        var alg = new HMACSHA1();
+        alg.Key = Encoding.UTF32.GetBytes("Pa$$w0rd");
+        byte[] data = Encoding.UTF8.GetBytes(doc);      
+        byte[] hash = alg.ComputeHash(data);
+        Console.WriteLine(Convert.ToBase64String(hash));
+        return hash;
+    }
+
+    private static bool Validate(string doc, byte[] hash)
+    {
+        var alg = SHA1.Create();
+        byte[] data = Encoding.UTF8.GetBytes(doc);
+        
+        byte[] hash2 = alg.ComputeHash(data);
+
+        if (hash.Length != hash2.Length) return false;
+        for(int i = 0; i < hash2.Length; i++) 
+        {
+            if (hash[i] != hash2[i]) return false;
+        }
+        return true;
+    }
+
+    private static byte[] GenerateHash(string doc)
+    {
+        var alg = SHA1.Create();
+        byte[] data = Encoding.UTF8.GetBytes(doc);
+        
+        byte[] hash = alg.ComputeHash(data);
+        Console.WriteLine(Convert.ToBase64String(hash));
+        return hash;
+    }
+}
---
+++ day3/Vertrouwelijk/Program.cs
@@ -0,0 +1,74 @@
+﻿
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Vertrouwelijk;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Asymmetrisch();
+        Symmetrisch();
+    }
+
+    private static void Symmetrisch()
+    {
+        // Zender
+        var algZender = Aes.Create();
+        var key = algZender.Key;
+        var iv = algZender.IV;
+        //algZender.Mode = CipherMode.CBC;
+
+        byte[] cipher;
+        using (MemoryStream mem = new MemoryStream())
+        {
+            using(CryptoStream cryp = new CryptoStream(mem, algZender.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                using (var writer = new StreamWriter(cryp))
+                {
+                    writer.WriteLine("Hello World");
+                }
+            }
+            cipher = mem.ToArray();
+        }
+        Console.WriteLine(Encoding.UTF8.GetString(cipher));
+
+        // Ontvanger
+        var algOntvanger = Aes.Create();
+        //algOntvanger.Mode = CipherMode.CBC;
+
+        algOntvanger.Key = key;
+        algOntvanger.IV = iv;
+
+        using(var mem2 = new MemoryStream(cipher))
+        {
+            using (var cryp2 = new CryptoStream(mem2, algOntvanger.CreateDecryptor(), CryptoStreamMode.Read))
+            {
+                using (var reader = new StreamReader(cryp2))
+                {
+                    var data = reader.ReadToEnd();
+                    Console.WriteLine(data);
+                }
+            }
+        }
+
+
+    }
+
+    private static void Asymmetrisch()
+    {
+        // Ontvanger heeft pub en priv key
+        var rsaOntvanger = RSA.Create();
+        string pubKey = rsaOntvanger.ToXmlString(false);
+
+        // Zender
+        var rsaZender = RSA.Create();
+        rsaZender.FromXmlString(pubKey);
+        var cipher = rsaZender.Encrypt(Encoding.UTF8.GetBytes("Hello World"), RSAEncryptionPadding.Pkcs1);
+    
+        // Ontvanger
+        var data = rsaOntvanger.Decrypt(cipher,RSAEncryptionPadding.Pkcs1);
+        Console.WriteLine(Encoding.UTF8.GetString(data));
+    }
+}
